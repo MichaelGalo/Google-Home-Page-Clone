b905179bf6df9848faf57f1e0d9d6dfa5ccab7a9
Michael Galo <michael@michaelthinks.co>
2024-04-25 11:47:05 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// const searchBtn = document.getElementById("search-btn");
+// const luckyBtn = document.getElementById("lucky-btn");
+
+// function redirectToNewTab(path) {
+//   const url = window.location.origin + "/" + path;
+//   window.open(url, "_blank");
+// }
+
+// searchBtn.addEventListener("click", function executeSearch() {
+//   redirectToNewTab("search.html");
+// });
+
+// luckyBtn.addEventListener("click", function executeSearch() {
+//   redirectToNewTab("lucky.html");
+// });
